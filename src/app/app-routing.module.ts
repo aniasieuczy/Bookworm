bb89bb8f335af65acbf61f661abce50ec660e460
Anna Szpecht <aszpecht@sii.pl>
2022-11-25 12:36:20 +0100

    routes to books and wishlist added

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,8 +2,10 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import {BooksComponent} from "./books/books.component";
 import {WishlistComponent} from "./wishlist/wishlist.component";
+import {HeaderComponent} from "./header/header.component";
 
 const routes: Routes = [
+  { path: '', redirectTo: '/books', pathMatch: "full" },
   { path: 'books', component: BooksComponent },
   { path: 'wishlist', component: WishlistComponent }
 ];
